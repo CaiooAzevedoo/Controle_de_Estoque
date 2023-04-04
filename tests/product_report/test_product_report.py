d5bc1ae19447dfda0ef204bb39a8df782c789e9a
Caio <caiusito@gmail.com>
2023-04-04 15:02:45 -0300

    Req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "produto",
+        "empresa",
+        "2023-04-04",
+        "2024-04-04",
+        "321",
+        "guardar",
+    )
+
+    message = """O produto produto fabricado em \
+2023-04-04 por empresa com \
+validade até 2024-04-04 precisa ser \
+armazenado guardar."""
+
+    assert str(product) == message
