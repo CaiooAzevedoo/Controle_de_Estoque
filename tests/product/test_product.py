edec26b6dea48f5f9f846a8f2f94e9d477628932
Caio <caiusito@gmail.com>
2023-04-04 14:50:37 -0300

    Req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
 from inventory_report.inventory.product import Product
+import datetime
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(1, 'empresa', 'produto', datetime.date(2023, 4, 4),
+                      datetime.date(2024, 4, 4), 321, 'guardar')
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'empresa'
+    assert product.nome_da_empresa == 'produto'
+    assert product.data_de_fabricacao == '2023-04-04'
+    assert product.data_de_validade == '2024-04-04'
+    assert product.numero_de_serie == 321
+    assert product.instrucoes_de_armazenamento == 'guardar'
