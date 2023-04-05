81894ef0bd2d04a6a321c49457d093391e8f484a
Caio <caiusito@gmail.com>
2023-04-05 15:40:04 -0300

    feat. Nomeando parâmetros test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,13 +3,18 @@ import datetime
 
 
 def test_cria_produto():
-    product = Product(1, 'empresa', 'produto', datetime.date(2023, 4, 4),
-                      datetime.date(2024, 4, 4), 321, 'guardar')
+    product = Product(
+        id=1,
+        nome_da_empresa='empresa',
+        nome_do_produto='produto',
+        data_de_fabricacao=datetime.date(2023, 4, 4),
+        data_de_validade=datetime.date(2024, 4, 4),
+        numero_de_serie=321,
+        instrucoes_de_armazenamento='guardar')
 
-    # orientação do teste pode estar invertido
     assert product.id == 1
-    assert product.nome_do_produto == 'empresa'
-    assert product.nome_da_empresa == 'produto'
+    assert product.nome_do_produto == 'produto'
+    assert product.nome_da_empresa == 'empresa'
     assert product.data_de_fabricacao == '2023-04-04'
     assert product.data_de_validade == '2024-04-04'
     assert product.numero_de_serie == 321
